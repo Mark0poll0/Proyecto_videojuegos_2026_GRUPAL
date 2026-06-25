25ca401c1e6e5d29a682272d4d7188e35756cb44
Enrique Julca <enrique.julca13@gmail.com>
2026-06-25 16:37:31 -0500

    Player Controller - Movimiento (Input System) + física + Orden de capas por Y

--- Assets/PROYECTO/CODE/Script/Player_Controller.cs
+++ Assets/PROYECTO/CODE/Script/Player_Controller.cs
@@ -1,6 +1,34 @@
 using UnityEngine;
 
-public class Player_Controller
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player_Controller : MonoBehaviour
 {
-    
+    [Header("Movimiento")]
+    [SerializeField] private float moveSpeed = 5f;
+
+    private PlayerController controls; // <- Clase generada por el Input System
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        controls = new PlayerController(); // Creamos el objeto del input
+    }
+
+    // Activar/desactivar el mapa de acciones cuando el objeto se esciende/apaga
+    private void OnEnable() => controls.Gameplay.Enable();
+    private void OnDisable() => controls.Gameplay.Disable();
+
+    private void Update()
+    {
+        // Leemos el Input en cada Frame (En Update, que va al ritmo de los fotogramas)
+        moveInput = controls.Gameplay.Move.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        // Movemos con Física en FixedUpdate (ritmo fijo del motor de física)
+        rb.linearVelocity = moveInput * moveSpeed;
+    }
 }
